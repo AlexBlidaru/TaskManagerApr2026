8004857105a06f9cf89875c8315c8f4c54a23002
AlexBlidaru <alexblidaru@yahoo.com>
2026-05-19 14:40:15 +0300

    Update script.js
    user roxana + add alti useri

--- script.js
+++ script.js
@@ -7,9 +7,12 @@ const USERS = [
   { id:'user1',     name:'Alex B',     pin:'1995', superUser:false  },
   { id:'user2',     name:'Giani C',    pin:'0000', superUser:true  },
   { id:'user3',     name:'MonelV',      pin:'1985', superUser:false },
-  { id:'user4',     name:'user4',      pin:'0000', superUser:false },
+  { id:'user4',     name:'RoxanaS',      pin:'2121', superUser:false },
   { id:'user5',     name:'user5',      pin:'0000', superUser:false },
   { id:'user6',     name:'user6',      pin:'0000', superUser:false },
+    { id:'user6',     name:'user7',      pin:'0000', superUser:false },
+    { id:'user6',     name:'user8',      pin:'0000', superUser:false },
+    { id:'user6',     name:'user9',      pin:'0000', superUser:false },
 ];
 
 /* ══ STATUS CONFIG ════════════════════════════ */
